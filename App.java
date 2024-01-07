1d6d50811dce04e7f6b637c9a92c834ee6dd10c2
Oliwia Kwiatkowska 4TP <149939773+OlKw4TP@users.noreply.github.com>
2024-01-07 01:07:16 +0100

    App.java

--- App.java
+++ App.java
@@ -1,2 +1,10 @@
+import javax.swing.*;
+
 public class App {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            MainMenu editor = new MainMenu();
+            editor.initGUI();
+        });
+    }
 }
